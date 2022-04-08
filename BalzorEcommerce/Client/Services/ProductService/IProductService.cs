82894d935bb26ab28c7f44e9f3d6acd1e1464cf4
Paulino Auyon <paulinoauyon@gmail.com>
2022-04-07 17:31:26 -0700

    Created service on client level

---
+++ BalzorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace BalzorEcommerce.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
